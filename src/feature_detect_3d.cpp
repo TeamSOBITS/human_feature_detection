d830da3b696c5c27018a8a224e6b616c3db8e9be
OnoFumiya <fumiyaono.choi@gmail.com>
2024-07-29 17:24:37 +0900

    Update ROS2

--- src/feature_detect_3d.cpp
+++ src/feature_detect_3d.cpp
@@ -1,16 +1,18 @@
 #include <stdio.h>
-#include <ros/ros.h>
+#include <rclcpp/rclcpp.hpp>
+// #include <ros/ros.h>
 #include <vector>
 #include <string>
 #include <iostream>
 #include <limits>
 #include <math.h>
 #include <cmath>
+#include <string>
 #include <cstring>
 #include <cstdlib>
 // #include <opencv2/opencv.hpp>
 // #include <cv_bridge/cv_bridge.h>
-#include <sensor_msgs/Image.h>
+#include <sensor_msgs/msg/image.hpp>
 
 #include "human_feature_detection/point_cloud_processor.hpp"
 #include "human_feature_detection/srv/feature3d.hpp"
@@ -24,7 +26,7 @@ struct RGB {
 class HUMAN_FEATURE_DETECT_3D {
     private:
         rclcpp::Node::SharedPtr nd_;
-        rclcpp::Subscription<hello_world_msgs::msg::Example>::SharedPtr sub_points_;
+        rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr sub_points_;
         // ros::NodeHandle nh_;
         // ros::NodeHandle pnh_;
         // ros::Subscriber sub_points_;
@@ -45,14 +47,14 @@ class HUMAN_FEATURE_DETECT_3D {
         float max_range;
         int bightness_value;
 
-        void cbPoints(const sensor_msgs::msg::PointCloud2::UniquePtr cloud_msg) {
+        void cbPoints(const std::shared_ptr<sensor_msgs::msg::PointCloud2> cloud_msg) {
         // void cbPoints(const sensor_msgs::PointCloud2ConstPtr &cloud_msg) {
             pcp_.transformFramePointCloud( target_frame_, cloud_msg, cloud_ );
             r.clear();
             g.clear();
             b.clear();
             double height_temp = (std::numeric_limits<double>::max())*(-1);
-            for (int i=0; i<cloud_->points.size(); i++)
+            for (std::size_t i=0; i<cloud_->points.size(); i++)
             {
                 if ((min_range < cloud_->points[i].x) && (cloud_->points[i].x < max_range) && (-0.3 < cloud_->points[i].y) && (cloud_->points[i].y < 0.3) && (0.0 < cloud_->points[i].z) && (cloud_->points[i].z < 2.0))
                 {
@@ -65,7 +67,7 @@ class HUMAN_FEATURE_DETECT_3D {
             if (height_temp != (std::numeric_limits<double>::max())*(-1))
             {
                 height = height_temp;
-                for (int i=0; i<cloud_->points.size(); i++)
+                for (std::size_t i=0; i<cloud_->points.size(); i++)
                 {
                     if ((min_range < cloud_->points[i].x) && (cloud_->points[i].x < max_range) && ((height - face_range - clothes_range) < cloud_->points[i].z ) && (cloud_->points[i].z < (height - face_range)))
                     {
@@ -86,43 +88,49 @@ class HUMAN_FEATURE_DETECT_3D {
         void human_feature_3d(const std::shared_ptr<human_feature_detection::srv::Feature3d::Request> req, std::shared_ptr<human_feature_detection::srv::Feature3d::Response> res)
         // bool human_feature_3d(human_feature_detection::Feature3d::Request &req, human_feature_detection::Feature3d::Response &res)
         {
-            min_range = req.min_range;
-            max_range = req.max_range;
+            min_range = req->min_range;
+            max_range = req->max_range;
             flag = false;
-            sub_points_ = nd->create_subscription<human_feature_detection::srv::Feature3d>(topic_name, 10, &HUMAN_FEATURE_DETECT_3D::cbPoints);
+            sub_points_ = nd_->create_subscription<sensor_msgs::msg::PointCloud2>(topic_name, 5, std::bind(&HUMAN_FEATURE_DETECT_3D::cbPoints, this, std::placeholders::_1));
             // sub_points_ = nh_.subscribe(topic_name, 5, &HUMAN_FEATURE_DETECT_3D::cbPoints, this);
             cloud_.reset(new PointCloud());
-            ros::Rate loop_rate(10);
+            rclcpp::Rate loop_rate(10);
             sleep(2.0);
-            while (ros::ok())
+            while (rclcpp::ok())
             {
                 if (flag)
                 {                    
                     break;
                 }
-                ros::spinOnce();
+                rclcpp::spin_some(nd_);
                 loop_rate.sleep();
             }
-            res.color = decideColor(set_rgb);
-            res.height = height*100;
-            ROS_INFO("R = %d, G = %d, B = %d",set_rgb.r + bightness_value, set_rgb.g + bightness_value, set_rgb.b + bightness_value);
-            ROS_INFO("color = %s",res.color.c_str());
-            ROS_INFO("height = %.1f\n", height*100);
-            if (res.color.c_str() == "Unknown")
+            res->color = decideColor(set_rgb);
+            res->height = height*100;
+            RCLCPP_INFO(nd_->get_logger(), "R = %d, G = %d, B = %d\n",set_rgb.r + bightness_value, set_rgb.g + bightness_value, set_rgb.b + bightness_value);
+            RCLCPP_INFO(nd_->get_logger(), "color = %s\n",res->color.c_str());
+            RCLCPP_INFO(nd_->get_logger(), "height = %.1f\n\n", height*100);
+            // ROS_INFO("R = %d, G = %d, B = %d\n",set_rgb.r + bightness_value, set_rgb.g + bightness_value, set_rgb.b + bightness_value);
+            // ROS_INFO("color = %s\n",res->color.c_str());
+            // ROS_INFO("height = %.1f\n\n", height*100);
+            if (res->color.compare("Unknown") == 0)
             {
-                res.color_success = false;
-                res.color = "";
+                res->color_success = false;
+                res->color = "";
             }
             else
             {
-                res.color_success = true;
+                res->color_success = true;
             }
-            sub_points_.shutdown();
-            return true;
+            // sub_points_.shutdown(nd_);
+            sub_points_ = nullptr;
         }
         void wait_for_call()
         {
-            rclcpp::Service<human_feature_detection::srv::Feature3d>::SharedPtr server_feature_3d = nd->create_service<std_srvs::srv::SetBool>("/human_feature_detection/feature3d", std::bind(&HUMAN_FEATURE_DETECT_3D::human_feature_3d, std::placeholders::_1, std::placeholders::_2));
+            rclcpp::Service<human_feature_detection::srv::Feature3d>::SharedPtr server_feature_3d;
+            server_feature_3d = nd_->create_service<human_feature_detection::srv::Feature3d>("/human_feature_detection/feature3d", std::bind(&HUMAN_FEATURE_DETECT_3D::human_feature_3d, this, std::placeholders::_1, std::placeholders::_2));
+            rclcpp::spin_some(nd_);
+            rclcpp::spin(nd_);
             // ros::ServiceServer server_feature_3d = nh_.advertiseService("/human_feature_detection/feature3d", &HUMAN_FEATURE_DETECT_3D::human_feature_3d, this);
             // ros::spinOnce();
             // ros::spin();
@@ -132,7 +140,6 @@ class HUMAN_FEATURE_DETECT_3D {
             float R = (float)((rgb.r + bightness_value) / 256.0);
             float G = (float)((rgb.g + bightness_value) / 256.0);
             float B = (float)((rgb.b + bightness_value) / 256.0);
-            printf("\n%f\n",R);
             double Max = std::max({ R, G, B });
             double Min = std::min({ R, G, B });
 
@@ -263,12 +270,18 @@ class HUMAN_FEATURE_DETECT_3D {
             }
         }
     public:
-        HUMAN_FEATURE_DETECT_3D(std::shared_ptr<rclcpp::Node> nd): nd_(nd) {
-            topic_name = nd_.param<std::string>( "topic_name", "/points2" );
-            face_range = nd_.param<float>( "face_range", 0.20 );
-            clothes_range = nd_.param<float>( "clothes_range", 0.50 );
-            target_frame_ = nd_.param<std::string>( "target_frame", "base_footprint" );
-            bightness_value = nd_.param<int>( "bightness_value", 0 );
+        HUMAN_FEATURE_DETECT_3D(std::shared_ptr<rclcpp::Node> nd): nd_(nd), pcp_(nd) {
+            // topic_name = nd_.param<std::string>( "topic_name", "/points2" );
+            // face_range = nd_.param<float>( "face_range", 0.20 );
+            // clothes_range = nd_.param<float>( "clothes_range", 0.50 );
+            // target_frame_ = nd_.param<std::string>( "target_frame", "base_footprint" );
+            // bightness_value = nd_.param<int>( "bightness_value", 0 );
+            topic_name = "/points2";
+            face_range = 0.20;
+            clothes_range = 0.50;
+            target_frame_ = "base_footprint";
+            bightness_value = 0;
+            // pcp_(nd_);
             wait_for_call();
         }
 };
@@ -277,6 +290,7 @@ int main(int argc, char **argv) {
     rclcpp::init(argc, argv);
     rclcpp::Node::SharedPtr nd = std::make_shared<rclcpp::Node>("human_feature_detection_3d");
     // ros::init(argc, argv, "human_feature_detection_3d");
-    HUMAN_FEATURE_DETECT_3D human_feature_detect_3d;
+    // HUMAN_FEATURE_DETECT_3D human_feature_detect_3d;
+    auto human_feature_detect_3d = std::make_shared<HUMAN_FEATURE_DETECT_3D>(nd);
     return 0;
 }
--- src/point_cloud_processor.cpp
+++ src/point_cloud_processor.cpp
@@ -2,29 +2,36 @@
 
 using namespace human_feature_detection;
 
-PointCloudProcessor::PointCloudProcessor() : tfBuffer_(), tf_listener_(tfBuffer_) {
-    tree_ .reset ( new pcl::search::KdTree<PointT>() );
+PointCloudProcessor::PointCloudProcessor(std::shared_ptr<rclcpp::Node> nd) : nd_(nd), tfBuffer_(std::make_shared<rclcpp::Clock>(RCL_ROS_TIME)), tf_listener_(tfBuffer_) {
+    tree_.reset ( new pcl::search::KdTree<PointT>() );
 }
-bool PointCloudProcessor::transformFramePointCloud(const std::string& target_frame, const sensor_msgs::PointCloud2ConstPtr& input_cloud, PointCloud::Ptr output_cloud) {
-    PointCloud cloud_src;
-    pcl::fromROSMsg(*input_cloud, cloud_src);
+bool PointCloudProcessor::transformFramePointCloud(const std::string& target_frame, const sensor_msgs::msg::PointCloud2::SharedPtr& input_cloud, PointCloud::Ptr output_cloud) {
+    PointCloud::Ptr cloud_src(new PointCloud);
+    pcl::fromROSMsg(*input_cloud, *cloud_src);
     if (!target_frame.empty()) {
         try {
-            while (ros::ok()) {
-                if (tfBuffer_.canTransform(target_frame, cloud_src.header.frame_id, ros::Time(0), ros::Duration(1.0))) {
+            while (rclcpp::ok()) {
+                if (tfBuffer_.canTransform(target_frame, input_cloud->header.frame_id, tf2::TimePointZero, tf2::durationFromSec(1.0))) {
+                // if (tfBuffer_.canTransform(target_frame, cloud_src.header.frame_id, ros::Time(0), ros::Duration(1.0))) {
                     break;
                 } else {
-                    ros::Duration(1.0).sleep();
+                    rclcpp::sleep_for(std::chrono::seconds(1));
+                    // ros::Duration(1).sleep();
                 }
             }
-            pcl_ros::transformPointCloud(target_frame, ros::Time(0), cloud_src, cloud_src.header.frame_id, *output_cloud, tfBuffer_);
+            geometry_msgs::msg::TransformStamped transform_stamped = tfBuffer_.lookupTransform(target_frame, input_cloud->header.frame_id, tf2::TimePointZero, tf2::durationFromSec(1.0));
+            pcl_ros::transformPointCloud(*cloud_src, *output_cloud, transform_stamped);
+            // pcl_ros::transformPointCloud(target_frame, tf2::TimePointZero, *cloud_src, input_cloud->header.frame_id, *output_cloud, tfBuffer_);
+            // pcl_ros::transformPointCloud(target_frame, ros::Time(0), cloud_src, cloud_src.header.frame_id, *output_cloud, tfBuffer_);
             output_cloud->header.frame_id = target_frame;
         } catch (tf2::TransformException& ex) {
-            ROS_ERROR("%s", ex.what());
+            RCLCPP_ERROR(nd_->get_logger(), "%s", ex.what());
+            // ROS_ERROR("%s", ex.what());
             return false;
         }
     } else {
-        ROS_ERROR("Please set the target frame.");
+        RCLCPP_ERROR(nd_->get_logger(), "Please set the target frame.");
+        // ROS_ERROR("Please set the target frame.");
         return false;
     }
     return true;
