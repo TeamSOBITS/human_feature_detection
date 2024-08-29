469b8103e0e64912e6f5aef43446b6597c36a429
MrKeith99 <kvalentincardenas@hotmail.com>
2024-08-29 12:03:50 +0900

    [FIX] install based on current path

--- model_setup.py
+++ model_setup.py
@@ -2,15 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import cv2
+import os
 from deepface import DeepFace
-import getpass
 
 def bbox(image, bboxes, name_list):
     for bbox, name in zip(bboxes, name_list):
         x, y, w, h = bbox
         cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
         cv2.putText(image, name, (x, y - 4), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-    path = "/home/" + str(getpass.getuser()) + "/colcon_ws/src/human_feature_detection/human_feature_detection"
+    path = os.path.dirname(os.path.realpath(__file__)) + "/human_feature_detection"
     cv2.imwrite(path + "/images/sample_image_result.png", image)
 
 def detect_faces(image):
@@ -27,7 +27,7 @@ def analyze_face(image, bbox):
 
 
 def main():
-    path = "/home/" + str(getpass.getuser()) + "/colcon_ws/src/human_feature_detection/human_feature_detection"
+    path = os.path.dirname(os.path.realpath(__file__)) + "/human_feature_detection"
     image = cv2.imread(path + "/images/sample_image.png")
     face_locations = detect_faces(image)
     print("\033[41m", face_locations, "\033[0m")
